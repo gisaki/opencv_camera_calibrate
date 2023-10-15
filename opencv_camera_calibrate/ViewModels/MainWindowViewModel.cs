0f4fb4da492040f02274a3e7e03873b0000020fb
gisaki <gisaki.dev@gmail.com>
2023-10-15 17:10:28 +0900

    Added opencv libraries and samples to check build

--- opencv_camera_calibrate/ViewModels/MainWindowViewModel.cs
+++ opencv_camera_calibrate/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using OpenCvSharp;
 
 namespace opencv_camera_calibrate.ViewModels
 {
@@ -28,6 +32,16 @@ namespace opencv_camera_calibrate.ViewModels
         // プロパティ（画面にbinding）
         public Models.SomeModel some_model_ { get; set; }
         public System.Collections.ObjectModel.ObservableCollection<Models.SomeModel> list_ { get; set; }
+        private BitmapSource _someImage;
+        public BitmapSource SomeImage
+        {
+            get { return this._someImage; }
+            set
+            {
+                // Setter で表示更新と連動させる仕組み
+                this.Set(ref this._someImage, value);
+            }
+        }
 
         // コマンド（画面にbinding）
         private Helpers.RelayCommand _doSomethingCommand;
@@ -46,9 +60,34 @@ namespace opencv_camera_calibrate.ViewModels
                 Models.SomeModel item = new Models.SomeModel() { TimeStamp = DateTime.Now.AddHours(1), };
                 this.list_.Add(item);
             }));
+            TestOpenCV();
         }
 
         // 操作
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);
+        private void TestOpenCV()
+        {
+            // 画像を読み込む
+            Mat src = new Mat(@"Parrots.jpg", ImreadModes.Grayscale);
+
+            // 二値化後画像
+            Mat dst = src.Clone();
+
+            // 二値化
+            Cv2.Threshold(src, dst, 0, 255, ThresholdTypes.Otsu);
+
+            // 表示する画像
+            Bitmap bitmap = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(dst);
+
+            // 表示
+            IntPtr hbitmap = bitmap.GetHbitmap();
+            this.SomeImage = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(hbitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            DeleteObject(hbitmap);
+
+            //メモリクリア
+            bitmap.Dispose();
+        }
 
     }
 }
